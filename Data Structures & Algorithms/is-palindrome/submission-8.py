fdab17b7a4f88bd0c2a17708179fe6293730be9f
Nyein Nyein Aye <allisonnyein@gmail.com>
2026-07-22 17:20:51 -0700

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) -1
+
+        while left < right:
+            while left<right and not self.isalnum(s[left]):
+                left += 1
+
+            while left<right and not self.isalnum(s[right]):
+                right -= 1
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+
+        return True
+
+    def isalnum(self, s):
+        return ord('A') <= ord (s) <= ord('Z') or ord('a') <= ord(s) <= ord('z') or ord('0') <= ord(s) <= ord('9')
+
+# Time = O(n) and Space = O(1)
