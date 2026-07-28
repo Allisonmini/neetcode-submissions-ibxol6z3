d8d7329b0f386808e32d6af8c5eeeb47c339af17
Nyein Nyein Aye <allisonnyein@gmail.com>
2026-07-28 09:45:39 -0700

    Add: duplicate-integer - submission-11

---
+++ Data Structures & Algorithms/duplicate-integer/submission-11.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+       mapping = set(nums)
+       return len(mapping) < len(nums)
+       ''' mapping = {}
+        for n in nums:
+            mapping[n] = mapping.get(n, 0) +1
+            if mapping[n]> 1:
+                return True
+        return False'''
